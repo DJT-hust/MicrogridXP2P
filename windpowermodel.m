5341b47013d9ce150f56162cba76ae883473e04a
DJT_hust <duanjuntao1@outlook.com>
2021-10-19 21:12:38 +0800

    3

--- windpowermodel.m
+++ windpowermodel.m
@@ -1,6 +1,6 @@
 function wind_power = windpowermodel(wind_speed) % 已知风速计算风功率！ 正确的！MW
     wind_power = zeros(1, 1);
-    para = 2 * 4^3 / (4^3 - 12.5^3);
+    para = 2 * 4^3 / (4^3 - 12.5^3);%123456
     parb = 2 / (12.5^3 - 4^3);
     temp_power = para + parb .* wind_speed.^3;
     wind_power(wind_speed > 4 & wind_speed <= 12.5) = temp_power(wind_speed > 4 & wind_speed <= 12.5);
